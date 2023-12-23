f9d6bf9e3ebb06dcddf62f77df9602140ce29001
Gereon V <72784429+GereonV@users.noreply.github.com>
2023-12-23 12:45:35 +0100

    Add win-conditions

--- Game.hs
+++ Game.hs
@@ -1,22 +1,30 @@
 module Game
     ( Board
+    , Game
     , GameResult(..)
     , Move
     , checkBoard
+    , getBoard
     , getMove
+    , getResult
+    , isP2Turn
     , move
-    , newBoard
+    , newGame
     , takeTurn
     ) where
 
 import Data.Bifunctor (first, second)
-import Data.Bits
+import Data.Bits ((.<<.), (.&.), (.|.), bit)
 import Data.Bool (bool)
 import Data.List (find, foldl')
-import Data.Maybe (fromJust)
+import Data.Maybe (fromJust, isNothing)
 import Data.Word (Word8, Word64)
 
-newtype Board = B (Word64, Word64, Bool)
+newtype Board = B (Word64, Word64)
+data Game = G
+    { getBoard  :: Board
+    , getResult :: Maybe GameResult
+    , isP2Turn  :: Bool }
 data GameResult = P1Won | Drawn | P2Won
   deriving (Bounded, Eq, Ord, Read, Show)
 newtype Move = M Word8
@@ -60,7 +68,22 @@ instance Enum Move where
             | otherwise = minBound
 
 checkBoard :: Board -> Maybe GameResult
-checkBoard = undefined
+checkBoard (B (p1, p2))
+    | p1 .|. p2 == bit (6 * 7) - 1 = Just Drawn
+    | won p1 = Just P1Won
+    | won p2 = Just P2Won
+    | otherwise = Nothing
+  where
+    bitwise t = foldl' (.|.) 0 $ bit . t <$> [0..3]
+    col = [shift row col colMask | col <- [0..6], row <- [0..2]]
+    colMask = bitwise (7 *)
+    diag = concat [shift row col <$> [diagMask1, diagMask2] | col <- [0..3], row <- [0..2]]
+    diagMask1 = bitwise (8 *)
+    diagMask2 = bitwise $ (3 +) . (6 *)
+    row = [shift row col rowMask | col <- [0..3], row <- [0..5]]
+    rowMask = bitwise id
+    shift r c = (.<<. (7 * r + c))
+    won p = any ((==) <*> (p .&.)) $ col ++ row ++ diag
 
 getMove :: Move -> Int
 getMove (M x) = fromEnum x
@@ -70,12 +93,14 @@ move x
     | minMove <= x && x <= maxMove = Just . M . toEnum $ x
     | otherwise = Nothing
 
-newBoard :: Board
-newBoard = B (0, 0, False)
+newGame :: Game
+newGame = G (curry B 0 0) Nothing False
 
-takeTurn :: Board -> Move -> Maybe Board
-takeTurn (B (p1, p2, p)) (M c) = B . uncurry (,, not p) . flip f (p1, p2) . (.|.) . bitIn <$> row
+takeTurn :: Game -> Move -> Maybe Game
+takeTurn (G (B ps) r p) (M c)
+    | isNothing r = ($ not p) . (G <*> checkBoard) . B . flip f ps . (.|.) . bitIn <$> row
+    | otherwise = Nothing
   where
     bitIn r = bit $ 7 * r + fromEnum c
     f = bool first second p
-    row = fromEnum <$> find (\r -> (p1 .|. p2) .&. bitIn r == 0) [0..5]
+    row = fromEnum <$> find (\r -> uncurry (.|.) ps .&. bitIn r == 0) [0..5]
